fb1013c9765b31c2fb3c3779b35de15cb0e712a9
Heso113 <hampe_olsson91@hotmail.com>
2019-03-21 09:06:54 +0100

    Fixed so that the player can't leave the screen

--- D3D12_Astroids/D3D12_Astroids/ComputeShaderTranslation.hlsl
+++ D3D12_Astroids/D3D12_Astroids/ComputeShaderTranslation.hlsl
@@ -1,16 +1,32 @@
-struct BufTypeTrans
-{
-	float x, y, z;
-};
-RWStructuredBuffer<BufTypeTrans> BufferPosition : register(u2);
-RWStructuredBuffer<BufTypeTrans> BufferDirection : register(u3);
+RWStructuredBuffer<float3> BufferPosition : register(u2);
+RWStructuredBuffer<float3> BufferDirection : register(u3);
 
 #define SPEED 1.0f
 
 [numthreads(1, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
-	BufferPosition[DTid.x].x += BufferDirection[DTid.x].x * BufferDirection[DTid.x].z * SPEED;
-	BufferPosition[DTid.x].y += BufferDirection[DTid.x].y * BufferDirection[DTid.x].z * SPEED;
+	float3 pos = BufferPosition[DTid.x];
+	float3 dir = BufferDirection[DTid.x];
+
+	pos.x += dir.x * dir.z * SPEED;
+	pos.y += dir.y * dir.z * SPEED;
+
+	if (pos.x < 20.0f && DTid.x == 0)
+		pos.x = 20.0f;
+
+	if (pos.x > 1580.0f && DTid.x == 0)
+		pos.x = 1580.0f;
+
+	if (pos.y < 20.0f && DTid.x == 0)
+		pos.y = 20.0f;
+	
+	if (pos.x > 880.0f && DTid.x == 0)
+		pos.y = 880.0f;
+
+	BufferPosition[DTid.x] = pos;
+
+	//BufferPosition[DTid.x].x += BufferDirection[DTid.x].x * BufferDirection[DTid.x].z * SPEED;
+	//BufferPosition[DTid.x].y += BufferDirection[DTid.x].y * BufferDirection[DTid.x].z * SPEED;
 	//BufferPosition[DTid.x].z += BufferDirection[DTid.x].z * BufferDirection[DTid.x].z;
 }
